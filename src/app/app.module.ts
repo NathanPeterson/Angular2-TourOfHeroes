9a91f1339fff6b5955329fd35f215f26f246592b
Nathan Peterson <nalexpeterson@gmail.com>
2016-12-01 16:02:32 -0600

    Add routing to the hero details from dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
