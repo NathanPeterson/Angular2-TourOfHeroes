956348d696f613fc0a08b3bbf2a231c33ba17c5e
Nathan Peterson <nalexpeterson@gmail.com>
2016-12-01 00:54:11 -0800

    Change hero from string to hero object. Display object in Html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heros';
-  hero = "Windstrorm";
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
